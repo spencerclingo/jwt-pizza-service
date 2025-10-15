0513a2c4af64f671dc061f081b39328d84fd7ee0
Spencer Clingo <spencerclingo@gmail.com>
2025-10-14 23:32:25 -0600

    Too much api endpoint logging is getting in the way

--- src/service.js
+++ src/service.js
@@ -17,7 +17,7 @@ app.use((req, res, next) => {
   next();
 });
 app.use((req, res, next) => {
-  console.log(`${req.method} ${req.originalUrl}`);
+  // console.log(`${req.method} ${req.originalUrl}`);
   next();
 });
 
