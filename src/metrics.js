111864d95edebf99dafed03830f2e732cdd90bf6
Spencer Clingo <spencerclingo@gmail.com>
2025-11-04 17:31:54 -0700

    Add grafana data functions

---
+++ src/metrics.js
@@ -0,0 +1,90 @@
+const config = require('./config');
+
+// Metrics stored in memory
+const requests = {};
+let greetingChangedCount = 0;
+
+// Function to track when the greeting is changed
+function greetingChanged() {
+    greetingChangedCount++;
+}
+
+// Middleware to track requests
+function requestTracker(req, res, next) {
+    const endpoint = `[${req.method}] ${req.path}`;
+    requests[endpoint] = (requests[endpoint] || 0) + 1;
+    next();
+}
+
+// This will periodically send metrics to Grafana
+setInterval(() => {
+    const metrics = [];
+    Object.keys(requests).forEach((endpoint) => {
+        metrics.push(createMetric('requests', requests[endpoint], '1', 'sum', 'asInt', { endpoint }));
+        metrics.push(createMetric('greetingChange', greetingChangedCount, '1', 'sum', 'asInt', {}));
+    });
+
+    sendMetricToGrafana(metrics);
+}, 10000);
+
+function createMetric(metricName, metricValue, metricUnit, metricType, valueType, attributes) {
+    attributes = { ...attributes, source: config.metrics.source };
+
+    const metric = {
+        name: metricName,
+        unit: metricUnit,
+        [metricType]: {
+            dataPoints: [
+                {
+                    [valueType]: metricValue,
+                    timeUnixNano: Date.now() * 1000000,
+                    attributes: [],
+                },
+            ],
+        },
+    };
+
+    Object.keys(attributes).forEach((key) => {
+        metric[metricType].dataPoints[0].attributes.push({
+            key: key,
+            value: { stringValue: attributes[key] },
+        });
+    });
+
+    if (metricType === 'sum') {
+        metric[metricType].aggregationTemporality = 'AGGREGATION_TEMPORALITY_CUMULATIVE';
+        metric[metricType].isMonotonic = true;
+    }
+
+    return metric;
+}
+
+function sendMetricToGrafana(metrics) {
+    const body = {
+        resourceMetrics: [
+            {
+                scopeMetrics: [
+                    {
+                        metrics,
+                    },
+                ],
+            },
+        ],
+    };
+
+    fetch(`${config.metrics.url}`, {
+        method: 'POST',
+        body: JSON.stringify(body),
+        headers: { Authorization: `Bearer ${config.metrics.apiKey}`, 'Content-Type': 'application/json' },
+    })
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error(`HTTP status: ${response.status}`);
+            }
+        })
+        .catch((error) => {
+            console.error('Error pushing metrics:', error);
+        });
+}
+
+module.exports = { requestTracker, greetingChanged };
